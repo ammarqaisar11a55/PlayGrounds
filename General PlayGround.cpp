29751220f327398c5ba55e376f47fd304e7459c4
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-01-29 02:14:52 +0500

    Create General PlayGround.cpp

---
+++ General PlayGround.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <unordered_set>
+#include <numeric>
+#include <functional>
+#include <cmath>
+#include <set>
+#include <chrono>
+#include <map>
+#include <list>
+
+using namespace std;
+
+void PrintVector(vector<string> a)
+{
+    cout << endl;
+    cout << "[";
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i];
+        if (i != a.size() - 1)
+        {
+            cout << ",";
+        }
+    }
+    cout << "]";
+    cout << endl;
+}
+
+void PrintVector(vector<char> a)
+{
+    cout << endl;
+    cout << "[";
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i];
+        if (i != a.size() - 1)
+        {
+            cout << ",";
+        }
+    }
+    cout << "]";
+    cout << endl;
+}
+
+void PrintVector(vector<int> a)
+{
+    cout << endl;
+    cout << "[";
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i];
+        if (i != a.size() - 1)
+        {
+            cout << ",";
+        }
+    }
+    cout << "]";
+    cout << endl;
+}
+
+void PrintMatrixVector(vector<vector<char>> Grid)
+{
+    for (int i = 0; i < Grid.size(); i++)
+    {
+        for (int j = 0; j < Grid[i].size(); j++)
+        {
+            cout << Grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+}
+
+void PrintMatrixVector(vector<vector<int>> Grid)
+{
+    for (int i = 0; i < Grid.size(); i++)
+    {
+        for (int j = 0; j < Grid[i].size(); j++)
+        {
+            cout << Grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+}
+
+void PrintMatrixVector(vector<vector<string>> Grid)
+{
+    for (int i = 0; i < Grid.size(); i++)
+    {
+        for (int j = 0; j < Grid[i].size(); j++)
+        {
+            cout << Grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+}
+
+/************************************************************/
+
+int main()
+{
+    auto start = chrono::high_resolution_clock::now();
+
+    // Test Cases Input:
+
+    cout << numTilePossibilities("AAB") << endl;
+    cout << numTilePossibilities("AAABBC") << endl;
+    cout << numTilePossibilities("V") << endl;
+
+    // Record the end time
+    auto end = chrono::high_resolution_clock::now();
+
+    // Calculate the duration in microseconds
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+    cout << "Execution time: " << duration << " ms" << endl;
+
+    return 0;
+}
