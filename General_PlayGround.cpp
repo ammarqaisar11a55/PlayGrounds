f273aef3d695fb810afdb2027f060d1414796cac
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-01-29 22:54:15 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -115,31 +115,38 @@ void PrintMatrixVector(vector<vector<string>> Grid)
 /*********************************************************************************************************/
 /*********************************************************************************************************/
 
-void Solve(int CurrentNumber, int limit,vector<int>&Res)
-{   
-    if(CurrentNumber>limit)
+void Solve(int CurrentNumber, int limit, vector<int> &Res)
+{
+    if (CurrentNumber > limit)
     {
         return;
     }
 
     Res.push_back(CurrentNumber);
 
-
-    for(int i = 0; i <= 9; i++)
+    for (int i = 0; i <= 9; i++)
     {
-        
-    }
+        int Next_Number = CurrentNumber * 10 + i;
 
+        if(Next_Number <= limit)
+        {
+            Solve(Next_Number,limit,Res);
+        }
+        else 
+        {
+            break;
+        }
+    }
 }
 
 vector<int> lexicalOrder(int n)
 {
-    vector<int>Res; 
+    vector<int> Res;
 
-    for(int i = 1; i <= 9; i++)
+    for (int i = 1; i <= 9; i++)
     {
-        Solve(i,n,Res);
-    } 
+        Solve(i, n, Res);
+    }
 
     return Res;
 }
@@ -152,7 +159,7 @@ int main()
 
     PrintVector(lexicalOrder(13));
     PrintVector(lexicalOrder(2));
-    
+
     /************************************************************************************/
 
     // Record the end time
