56bca4d7b08ddfde38b0d0e0481cb279060489f3
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-01-29 20:19:16 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -115,27 +115,89 @@ void PrintMatrixVector(vector<vector<string>> Grid)
 /*********************************************************************************************************/
 /*********************************************************************************************************/
 
-void Test()
+int FindingParents(vector<int> &Parents, int Node)
 {
-    cout<<"Hello World"<<endl;
+    if (Parents[Node] == Node)
+    {
+        return Node;
+    }
+
+    return Parents[Node] = FindingParents(Parents, Parents[Node]);
+}
+
+void UnionSet(int U, int V, vector<int> &Parents, vector<int> &Ranks)
+{
+    int U_Parent = FindingParents(Parents, U);
+    int V_Parent = FindingParents(Parents, V);
+
+    if (U_Parent != V_Parent)
+    {
+        if (Ranks[U_Parent] < Ranks[V_Parent])
+        {
+            Parents[U_Parent] = V_Parent;
+        }
+        else if (Ranks[U_Parent] > Ranks[V_Parent])
+        {
+            Parents[V_Parent] = U_Parent;
+        }
+        else
+        {
+            Parents[U_Parent] = V_Parent;
+            Ranks[V_Parent]++;
+        }
+    }
+}
+
+void Kruskals_Algorithm(vector<vector<int>> Edges, int Total_Nodes)
+{
+    vector<int> Parents(Total_Nodes);
+    vector<int> Ranks(Total_Nodes, 0);
+
+    for (int i = 0; i < Total_Nodes; i++)
+    {
+        Parents[i] = i;
+    }
+
+    sort(Edges.begin(), Edges.end(), [](vector<int> A, vector<int> B)
+         { return A[2] < B[2]; });
+
+    int Total_Minimum_Weight = 0;
+
+    for (int i = 0; i < Edges.size(); i++)
+    {
+        int Parent_of_U = FindingParents(Parents, Edges[i][0]);
+        int Parent_of_V = FindingParents(Parents, Edges[i][1]);
+        int Weight = Edges[i][2];
+
+        if (Parent_of_U != Parent_of_V)
+        {
+            Total_Minimum_Weight += Weight;
+            UnionSet(Edges[i][0], Edges[i][1], Parents, Ranks);
+        }
+    }
+
+    cout << Total_Minimum_Weight << endl;
+
+    // Test Cases:
+
+    // vector<vector<int>> Edges;
+
+    // Edges.push_back({0, 1, 10});
+    // Edges.push_back({0, 2, 6});
+    // Edges.push_back({0, 3, 5});
+    // Edges.push_back({1, 3, 15});
+    // Edges.push_back({2, 3, 4});
+
+    // Kruskals_Algorithm(Edges, 4);
 }
 
 int main()
 {
     auto start = chrono::high_resolution_clock::now();
 
-
     /************************************** Input Test Cases: **************************/
 
-    Test();
-    
-
-
-
-
     /************************************************************************************/
-
-    
 
     // Record the end time
     auto end = chrono::high_resolution_clock::now();
