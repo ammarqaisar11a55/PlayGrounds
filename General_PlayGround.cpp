ff308d692d3ff8acba63bcff2138ab71573bcdb8
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-01-31 21:39:50 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -131,7 +131,7 @@ int main()
     /************************************** Input Test Cases: **************************/
 
     cout<<largestIsland(vector<vector<int>>{[1,0],[0,1]})<<endl;
-    cout<<largestIsland(vector<vector<int>>{})<<endl;
+    cout<<largestIsland(vector<vector<int>>{[1,1],[1,0]})<<endl;
     cout<<largestIsland(vector<vector<int>>{})<<endl;
 
     /************************************************************************************/
