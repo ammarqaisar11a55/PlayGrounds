5ecf3430d7691c790c62db63b13cb937830a354a
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-01-31 23:32:32 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -120,81 +120,13 @@ void PrintMatrixVector(vector<vector<string>> Grid)
 /*********************************************************************************************************/
 /*********************************************************************************************************/
 
-void DFS(vector<vector<int>> Grid, int i, int j, int Count_Ones, int &Res, bool Changed_Zero_To_One)
-{
-    if (i < 0 || j < 0 || i >= Grid.size() || j >= Grid[0].size())
-        return;
 
-    if (Grid[i][j] == 0 && Changed_Zero_To_One == true)
-        return;
-
-    int temp = Grid[i][j];
-    Grid[i][j] = 0;
-
-    if (temp == 0)
-    {
-        Changed_Zero_To_One = true;
-    }
-
-    Res = max(Res, Count_Ones);
-
-    DFS(Grid, i + 1, j, Count_Ones + 1, Res, Changed_Zero_To_One);
-    DFS(Grid, i - 1, j, Count_Ones + 1, Res, Changed_Zero_To_One);
-    DFS(Grid, i, j + 1, Count_Ones + 1, Res, Changed_Zero_To_One);
-    DFS(Grid, i, j - 1, Count_Ones + 1, Res, Changed_Zero_To_One);
-
-    Grid[i][j] = temp;
-    Changed_Zero_To_One = false;
-}
-
-int largestIsland(vector<vector<int>> grid)
-{
-    int res = 0;
-    int Count_Zeros = 0;
-
-    for (int i = 0; i < grid.size(); i++)
-    {
-        for (int j = 0; j < grid[i].size(); j++)
-        {
-            if (grid[i][j] == 1)
-            {
-                DFS(grid, i, j, 1, res, false);
-            }
-            else
-                Count_Zeros++;
-        }
-    }
-
-    if (Count_Zeros == 0)
-    {
-        return grid.size() * grid.size();
-    }
-
-    return res == 0 ? res + 1 : res;
-}
 
 int main()
 {
     auto start = chrono::high_resolution_clock::now();
 
     /************************************** Input Test Cases: **************************/
-
-    cout << largestIsland(vector<vector<int>>{{1, 0}, {0, 1}}) << endl;
-    cout << largestIsland(vector<vector<int>>{{1, 1}, {1, 0}}) << endl;
-    cout << largestIsland(vector<vector<int>>{{1, 1}, {1, 1}}) << endl;
-    cout << largestIsland(vector<vector<int>>{{0, 0}, {0, 1}}) << endl;
-    cout << largestIsland(vector<vector<int>>{{0, 0}, {0, 0}}) << endl;
-
-    vector<vector<int>> grid = {
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 1, 1, 1, 1, 0, 0},
-        {0, 1, 0, 0, 1, 0, 0},
-        {1, 0, 1, 0, 1, 0, 0},
-        {0, 1, 0, 0, 1, 0, 0},
-        {0, 1, 0, 0, 1, 0, 0},
-        {0, 1, 1, 1, 1, 0, 0}};
-
-    cout << largestIsland(grid) << endl;
 
     /************************************************************************************/
 
