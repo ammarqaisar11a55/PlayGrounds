a65dff90407a804c2278afcf5c0d8f95e1383f8b
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-01 21:15:05 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -122,29 +122,7 @@ void PrintMatrixVector(vector<vector<string>> Grid)
 
 vector<int> pivotArray(vector<int> nums, int pivot)
 {
-    vector<int> Res;
-    int Count_Elements_Equal_to_Pivot = count(nums.begin(), nums.end(), pivot);
-
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (nums[i] < pivot)
-        {
-            Res.push_back(nums[i]);
-        }
-    }
-
-    for (int i = 0; i < Count_Elements_Equal_to_Pivot; i++)
-        Res.push_back(pivot);
-
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (nums[i] > pivot)
-        {
-            Res.push_back(nums[i]);
-        }
-    }
-
-    return Res;
+    int 
 }
 
 int main()
