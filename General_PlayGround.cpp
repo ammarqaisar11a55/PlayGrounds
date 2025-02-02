6ea1bd0d21bbd2586e1829ba06b697df7484085c
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-03 01:06:26 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -144,15 +144,23 @@ string findValidPair(string s)
     for (char c : s)
         mp[c]++;
 
+    string res;
+
     for (int i = 0; i < s.length() - 1; i++)
     {
         if (s[i] != s[i + 1])
         {
-            
+            if (mp[s[i]] == s[i] - '0' && mp[s[i + 1]] == s[i + 1] - '0')
+            {
+                res += s[i];
+                res += s[i + 1];
+
+                return res;
+            }
         }
     }
 
-    return "";
+    return res;
 }
 
 int main()
