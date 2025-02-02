3dec2a3a785e535ddec1fb62fd8595ea7ffba8b9
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-03 01:16:31 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -137,41 +137,13 @@ void PrintMatrixVector(vector<vector<string>> Grid)
 /*********************************************************************************************************/
 /*********************************************************************************************************/
 
-string findValidPair(string s)
-{
-    unordered_map<char, int> mp;
 
-    for (char c : s)
-        mp[c]++;
-
-    string res;
-
-    for (int i = 0; i < s.length() - 1; i++)
-    {
-        if (s[i] != s[i + 1])
-        {
-            if (mp[s[i]] == s[i] - '0' && mp[s[i + 1]] == s[i + 1] - '0')
-            {
-                res += s[i];
-                res += s[i + 1];
-
-                return res;
-            }
-        }
-    }
-
-    return res;
-}
 
 int main()
 {
     auto start = chrono::high_resolution_clock::now();
 
     /************************************** Input Test Cases: **************************/
-
-    cout << findValidPair("2523533") << endl;
-    cout << findValidPair("221") << endl;
-    cout << findValidPair("22s") << endl;
 
     /************************************************************************************/
 
