2903ab064eeb5bcc813f163384996acc07877ea5
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-03 17:42:08 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -137,46 +137,8 @@ void PrintMatrixVector(vector<vector<string>> Grid)
 /*********************************************************************************************************/
 /*********************************************************************************************************/
 
-int longestMonotonicSubarray(vector<int> nums)
+int minimumPushes(string word)
 {
-    int longest_increasing = 1;
-    int longest_decreasing = 1;
-
-    int res = 0;
-
-    for (int i = 0; i < nums.size() - 1; i++)
-    {
-        if (nums[i] < nums[i + 1])
-        {
-            longest_increasing++;
-        }
-        else
-        {
-            res = max(res, longest_increasing);
-            longest_increasing = 1;
-        }
-
-        res = max(res, longest_increasing);
-    }
-
-    for (int i = 0; i < nums.size() - 1; i++)
-    {
-        if (nums[i] > nums[i + 1])
-        {
-            longest_decreasing++;
-        }
-        else
-        {
-            res = max(res, longest_decreasing);
-            longest_decreasing = 1;
-        }
-
-        res = max(res, longest_decreasing);
-    }
-
-    
-
-    return res;
 }
 
 int main()
@@ -184,10 +146,6 @@ int main()
     auto start = chrono::high_resolution_clock::now();
 
     /************************************** Input Test Cases: **************************/
-
-    cout << longestMonotonicSubarray(vector<int>{1, 4, 3, 3, 2}) << endl;
-    cout << longestMonotonicSubarray(vector<int>{3, 3, 3, 3}) << endl;
-    cout << longestMonotonicSubarray(vector<int>{3, 2, 1}) << endl;
 
     /************************************************************************************/
 
