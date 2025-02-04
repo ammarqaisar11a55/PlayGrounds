22215d45906b590b115cef97d0113e8497d0434b
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-04 19:37:33 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -137,41 +137,11 @@ void PrintMatrixVector(vector<vector<string>> Grid)
 /*********************************************************************************************************/
 /*********************************************************************************************************/
 
-int maxAscendingSum(vector<int> nums)
-{
-    int i = 0;
-    int res = 0;
-    int single_max = INT_MIN;
-
-    while (i < nums.size())
-    {
-        int sum = nums[i];
-        single_max = max(nums[i],single_max);
-
-        while (i + 1 < nums.size() && nums[i] < nums[i + 1])
-        {
-            sum += nums[i + 1];
-            single_max = max(nums[i],single_max);
-            i++;
-        }
-        single_max = max(nums[i],single_max);
-        res = max(res, sum);
-        i++;
-    }
-
-    return max(res,single_max);
-}
-
 int main()
 {
     auto start = chrono::high_resolution_clock::now();
 
     /************************************** Input Test Cases: **************************/
-
-    cout << maxAscendingSum(vector<int>{10, 20, 30, 5, 10, 50}) << endl;
-    cout << maxAscendingSum(vector<int>{10, 20, 30, 40, 50}) << endl;
-    cout << maxAscendingSum(vector<int>{12, 17, 15, 13, 10, 11, 12}) << endl;
-    cout << maxAscendingSum(vector<int>{6, 10, 6}) << endl;
 
     /************************************************************************************/
 
