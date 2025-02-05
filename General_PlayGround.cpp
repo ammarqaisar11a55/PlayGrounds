abc8e5cbf3672fe136177ada7ecebc9b80b348cd
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-05 16:34:52 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -13,7 +13,20 @@
 #include <list>
 using namespace std;
 
+class TreeNode
+{
+public:
+    TreeNode *left;
+    TreeNode *right;
+    int val;
 
+    TreeNode(int d)
+    {
+        left = nullptr;
+        right = nullptr;
+        val = d;
+    }
+};
 
 void PrintVector(vector<string> a)
 {
@@ -157,7 +170,7 @@ void Level_Order_Traversal(TreeNode *Root)
         }
         else
         {
-            cout << Temp->Data << " ";
+            cout << Temp->val << " ";
 
             if (Temp->left != NULL)
                 Nodes.push(Temp->left);
@@ -168,9 +181,9 @@ void Level_Order_Traversal(TreeNode *Root)
     }
 }
 
-TreeNode *createBinaryTree(vector<vector<int>> &descriptions)
+TreeNode *createBinaryTree(vector<vector<int>> descriptions)
 {
-
+    return nullptr;
 }
 
 int main()
@@ -178,7 +191,8 @@ int main()
     auto start = chrono::high_resolution_clock::now();
 
     /************************************** Input Test Cases: **************************/
-    Level_Order_Traversal(createBinaryTree())
+    Level_Order_Traversal(createBinaryTree(vector<vector<int>>{{20, 15, 1}, {20, 17, 0}, {50, 20, 1}, {50, 80, 0}, {80, 19, 1}}));
+    Level_Order_Traversal(createBinaryTree(vector<vector<int>>{{1, 2, 1}, {2, 3, 0}, {3, 4, 1}}));
     /************************************************************************************/
 
     // Record the end time
