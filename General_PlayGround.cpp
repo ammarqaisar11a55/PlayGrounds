9dd90619b8060a5a3fb4db4157118a46575c8f6b
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-07 19:58:53 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -190,7 +190,18 @@ vector<int> queryResults(int limit, vector<vector<int>> queries)
 
     for(vector<int>Q : queries)
     {
-        
+        int Ball = Q[0];
+        int Color = Q[1];
+
+        if(Balls_with_Colors.find(Ball) == Balls_with_Colors.end())
+        {
+            Balls_with_Colors[Ball] = Color;
+            Current_Colors_Used.find
+        }
+
+
+
+        Res.push_back(Balls_with_Colors.size());
     }
 
 
