abb9d61ba3fede1abc2233304a9745e5488b3d00
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2025-02-19 09:02:36 +0500

    Update General_PlayGround.cpp

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -124,39 +124,76 @@ void PrintMatrixVector(vector<vector<int>> Grid)
     cout << endl;
 }
 
-/*********************************************************************************************************/
-/*********************************************************************************************************/
-/*********************************************************************************************************/
-/*********************************************************************************************************/
-/*********************************************************************************************************/
-/*********************************************************************************************************/
-/*********************************************************************************************************/
-
-void Solve(vector<int> Current_Combination, vector<vector<int>> &Res, int k, int range, int start)
+void PrintMatrixVector(vector<vector<string>> Grid)
 {
-    if (Current_Combination.size() == k)
+    for (int i = 0; i < Grid.size(); i++)
     {
-        if (Current_Combination.empty() == false)
+        for (int j = 0; j < Grid[i].size(); j++)
         {
-            Res.push_back(Current_Combination);
+            cout << Grid[i][j] << " ";
         }
+        cout << endl;
+    }
 
+    cout << endl;
+}
+
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+
+void Solve(int Current_Row, int Queens, int N_RS_CLS, vector<string> Board, vector<vector<string>> &Res, unordered_set<int> &Used_Rows, unordered_set<int> &Used_Cols, unordered_set<int> &Used_Di, unordered_set<int> &Used_AntiDi)
+{
+    if (Queens == 0)
+    {
+        Res.push_back(Board);
         return;
     }
 
-    for(int i = start; i <= range; i++)
+    for (int i = Current_Row; i < N_RS_CLS; i++)
     {
-        Current_Combination.push_back(i);
-        Solve(Current_Combination,Res,k,range,i + 1);
-        Current_Combination.pop_back();
+        for (int j = 0; j < N_RS_CLS; j++)
+        {
+            if (Used_Rows.find(i) != Used_Rows.end())
+                continue;
+
+            if (Used_Cols.find(j) != Used_Cols.end())
+                continue;
+
+            if (Used_Di.find(i - j) != Used_Di.end())
+                continue;
+
+            if (Used_AntiDi.find(i + j) != Used_AntiDi.end())
+                continue;
+
+            Board[i][j] = 'Q';
+            Used_Rows.insert(i);
+            Used_Cols.insert(j);
+            Used_Di.insert(i - j);
+            Used_AntiDi.insert(i + j);
+            
+            Solve(i + 1, Queens - 1, N_RS_CLS, Board, Res, Used_Rows, Used_Cols, Used_Di, Used_AntiDi);
+            
+            Board[i][j] = '.';
+            Used_Rows.erase(i);
+            Used_Cols.erase(j);
+            Used_Di.erase(i - j);
+            Used_AntiDi.erase(i + j);
+        }
     }
 }
 
-vector<vector<int>> combine(int n, int k)
+vector<vector<string>> solveNQueens(int n)
 {
-    vector<vector<int>> Res;
-    Solve({}, Res, k, n,1);
-     return Res;
+    vector<vector<string>> Res;
+    unordered_set<int> Used_Rows, Used_Cols, Used_Di, Used_AntiDi;
+    vector<string> Board(n, string(n, '.'));
+    Solve(0,n,n,Board,Res,Used_Rows,Used_Cols,Used_Di,Used_AntiDi);
+    return Res;
 }
 
 int main()
@@ -167,8 +204,8 @@ int main()
 
     // freopen("Output.txt", "w", stdout);
 
-    PrintMatrixVector(combine(4, 2));
-    PrintMatrixVector(combine(1, 1));
+    PrintMatrixVector(solveNQueens(4));
+    PrintMatrixVector(solveNQueens(1));
 
     /************************************************************************************/
     /************************************************************************************/
