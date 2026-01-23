4a11430a7bcd548af457430b5e6940de2891839f
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2026-01-23 17:04:59 +0500

    a

--- General_PlayGround.cpp
+++ General_PlayGround.cpp
@@ -154,9 +154,40 @@ void PrintList(ListNode *Head)
     g++ General_PlayGround.cpp -o Gp
 */
 
+int numRescueBoats(vector<int> people, int limit)
+{
+    sort(people.begin(),people.end());
 
+    int res = 0;
 
+    int left = 0, right = people.size() - 1;
 
+    int current_board = limit;
+
+    while(left<right)
+    {
+        if(current_board - people[right] >= 0)
+        {
+            current_board -= people[right];
+            right--;
+            continue;
+        }
+
+        if(current_board - people[left] >= 0)
+        {
+            current_board -= people[left];
+            left++;
+            continue;
+        }
+        
+        res++;
+        current_board = limit;
+        right --;
+        
+    }
+
+    return res + 1;
+}
 
 int main()
 {
@@ -164,7 +195,11 @@ int main()
 
     /************************************** Input Test Cases: **************************/
     /************************************************************************************/
-
+    cout<<"Expected:  1, " << numRescueBoats(vector<int>{1, 2}, 3) << endl;
+    cout<<"Expected:  3, " << numRescueBoats(vector<int>{3,2,2,1}, 3) << endl;
+    cout<<"Expected:  4, " << numRescueBoats(vector<int>{3,5,3,4}, 5) << endl;
+    cout<<"Expected:  2, " << numRescueBoats(vector<int>{2,4}, 5) << endl;
+    cout<<"Expected:  2, " << numRescueBoats(vector<int>{5,1,4,2}, 6) << endl;
     /************************************************************************************/
     /************************************************************************************/
     /************************************************************************************/
